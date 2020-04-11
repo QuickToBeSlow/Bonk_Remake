e0644c181cddb4e405584a20ada1c5ded3f174d8
Blake <35315543+QuickToBeSlow@users.noreply.github.com>
2020-04-11 12:49:03 -0500

    Editing world for player control.

--- examples/b2jsTestStack.js
+++ examples/b2jsTestStack.js
@@ -1,5 +1,7 @@
 (function(){
 
+var ctx = document.getElementById("canvas");
+
 var Test = function() {
 	b2jsTest.__constructor.apply(this, arguments);
 };
@@ -21,37 +23,38 @@ Test.prototype.createWorld = function() {
 	fd.shape = sd;
 	var i;
 	// Create 3 stacks
-	for (i = 0; i < 10; i++){
-		sd.SetAsBox((10) / m_physScale, (10) / m_physScale);
-		bd.position.Set((640/2+0+Math.random()*2 - 1) / m_physScale, (360-5-i*25) / m_physScale);
-		b = world.CreateBody(bd);
-		b.CreateFixture(fd);
-	}
-	for (i = 0; i < 10; i++){
-		sd.SetAsBox((10) / m_physScale, (10) / m_physScale);
-		bd.position.Set((640/2+100+Math.random()*1 - 0.5) / m_physScale, (360-5-i*25) / m_physScale);
-		b = world.CreateBody(bd);
-		b.CreateFixture(fd);
-	}
-	for (i = 0; i < 10; i++){
-		sd.SetAsBox((10) / m_physScale, (10) / m_physScale);
-		bd.position.Set((640/2+200+Math.random()*0.5 - 0.25) / m_physScale, (360-5-i*25) / m_physScale);
-		b = world.CreateBody(bd);
-		b.CreateFixture(fd);
-	}
+	// for (i = 0; i < 10; i++){
+	// 	sd.SetAsBox((10) / m_physScale, (10) / m_physScale);
+	// 	bd.position.Set((640/2+0+Math.random()*2 - 1) / m_physScale, (360-5-i*25) / m_physScale);
+	// 	b = world.CreateBody(bd);
+	// 	b.CreateFixture(fd);
+	// }
+	// for (i = 0; i < 10; i++){
+	// 	sd.SetAsBox((10) / m_physScale, (10) / m_physScale);
+	// 	bd.position.Set((640/2+100+Math.random()*1 - 0.5) / m_physScale, (360-5-i*25) / m_physScale);
+	// 	b = world.CreateBody(bd);
+	// 	b.CreateFixture(fd);
+	// }
+	// for (i = 0; i < 10; i++){
+	// 	sd.SetAsBox((10) / m_physScale, (10) / m_physScale);
+	// 	bd.position.Set((640/2+200+Math.random()*0.5 - 0.25) / m_physScale, (360-5-i*25) / m_physScale);
+	// 	b = world.CreateBody(bd);
+	// 	b.CreateFixture(fd);
+	// }
 	// Create ramp
 
-	// var vxs = [new b2Vec2(0, 0),
+	var vxs = [new b2Vec2(0, 0),
 		
-	// 	new b2Vec2(200 / m_physScale, 0),
-	// 	new b2Vec2(0, 100 / m_physScale)];
-	// sd.SetAsArray(vxs, vxs.length);
-	// fd.density = 0;
-	// bd.type = b2Body.b2_staticBody;
-	// bd.userData = "ramp";
-	// bd.position.Set(0, 0);
-	// b = world.CreateBody(bd);
-	// b.CreateFixture(fd);
+		new b2Vec2(200 / m_physScale, 0),
+		new b2Vec2(200 / m_physScale, 100 / m_physScale),
+		new b2Vec2(0, 100 / m_physScale)];
+	sd.SetAsArray(vxs, vxs.length);
+	fd.density = 0;
+	bd.type = b2Body.b2_staticBody;
+	bd.userData = "ramp";
+	bd.position.Set(0, 0);
+	b = world.CreateBody(bd);
+	b.CreateFixture(fd);
 
 	// Create ball
 	var P1 = new b2CircleShape();
@@ -63,12 +66,13 @@ Test.prototype.createWorld = function() {
 	bd.type = b2Body.b2_dynamicBody;
 	bd.userData = "ball";
 	bd.isBullet = true;
-	bd.position.Set(50/m_physScale, 140 / m_physScale);
+	bd.position.Set(ctx.style.width*0.75/m_physScale, 140 / m_physScale);
 	b = world.CreateBody(bd);
 	b.CreateFixture(fd);
 
 	var P2 = new b2CircleShape();
 	P2.m_radius = 15/m_physScale;
+	bd.position.Set(ctx.style.width*0.25/m_physScale, 140 / m_physScale);
 	b = world.CreateBody(bd);
 	b.CreateFixture(fd);
 
