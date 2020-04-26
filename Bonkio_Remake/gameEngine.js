c546ae8dcb98affc4d3dbf6f1f3f375d88010d84
Blake <35315543+QuickToBeSlow@users.noreply.github.com>
2020-04-26 09:39:12 -0500

    fixing bugs.

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -84,73 +84,7 @@ class Bird {
 	//   this.y += this.velocity;
 	}
   }
-
-// function keyPressed() {
-//   if (key === 'S') {
-//     let bird = birds[0];
-//     saveJSON(bird.brain, 'bird.json');
-//   }
-// }
-
-  tf.setBackend('cpu');
-  for (let i = 0; i < TOTAL; i++) {
-    birds[i] = new Bird();
-  }
-
-// function draw() {
-//   for (let n = 0; n < slider.value(); n++) {
-//     if (counter % 75 == 0) {
-//       pipes.push(new Pipe());
-//     }
-//     counter++;
-
-//     for (let i = pipes.length - 1; i >= 0; i--) {
-//       pipes[i].update();
-
-//       for (let j = birds.length - 1; j >= 0; j--) {
-//         if (pipes[i].hits(birds[j])) {
-//           savedBirds.push(birds.splice(j, 1)[0]);
-//         }
-//       }
-
-//       if (pipes[i].offscreen()) {
-//         pipes.splice(i, 1);
-//       }
-//     }
-
-//     for (let i = birds.length - 1; i >= 0; i--) {
-//       if (birds[i].offScreen()) {
-//         savedBirds.push(birds.splice(i, 1)[0]);
-//       }
-//     }
-
-//     for (let bird of birds) {
-//       bird.think(pipes);
-//       bird.update();
-//     }
-
-//     if (birds.length === 0) {
-//       counter = 0;
-//       nextGeneration();
-//       pipes = [];
-//     }
-//   }
-
-//   // All the drawing stuff
-//   background(0);
-
-//   for (let bird of birds) {
-//     bird.show();
-//   }
-
-//   for (let pipe of pipes) {
-//     pipe.show();
-//   }
-// }
-
-
-
-class NeuralNetwork {
+  class NeuralNetwork {
 	constructor(a, b, c, d) {
 	  if (a instanceof tf.Sequential) {
 		this.model = a;
@@ -234,6 +168,73 @@ class NeuralNetwork {
 	  return model;
 	}
   }
+
+// function keyPressed() {
+//   if (key === 'S') {
+//     let bird = birds[0];
+//     saveJSON(bird.brain, 'bird.json');
+//   }
+// }
+
+  tf.setBackend('cpu');
+  for (let i = 0; i < TOTAL; i++) {
+    birds[i] = new Bird();
+  }
+
+// function draw() {
+//   for (let n = 0; n < slider.value(); n++) {
+//     if (counter % 75 == 0) {
+//       pipes.push(new Pipe());
+//     }
+//     counter++;
+
+//     for (let i = pipes.length - 1; i >= 0; i--) {
+//       pipes[i].update();
+
+//       for (let j = birds.length - 1; j >= 0; j--) {
+//         if (pipes[i].hits(birds[j])) {
+//           savedBirds.push(birds.splice(j, 1)[0]);
+//         }
+//       }
+
+//       if (pipes[i].offscreen()) {
+//         pipes.splice(i, 1);
+//       }
+//     }
+
+//     for (let i = birds.length - 1; i >= 0; i--) {
+//       if (birds[i].offScreen()) {
+//         savedBirds.push(birds.splice(i, 1)[0]);
+//       }
+//     }
+
+//     for (let bird of birds) {
+//       bird.think(pipes);
+//       bird.update();
+//     }
+
+//     if (birds.length === 0) {
+//       counter = 0;
+//       nextGeneration();
+//       pipes = [];
+//     }
+//   }
+
+//   // All the drawing stuff
+//   background(0);
+
+//   for (let bird of birds) {
+//     bird.show();
+//   }
+
+//   for (let pipe of pipes) {
+//     pipe.show();
+//   }
+// }
+
+
+
+
 
 // Daniel Shiffman
 // Neuro-Evolution Flappy Bird
