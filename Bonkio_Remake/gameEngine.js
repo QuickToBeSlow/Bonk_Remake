75c6ad0004ea8d2834270deac42e97ea3d8986bf
Blake <35315543+QuickToBeSlow@users.noreply.github.com>
2020-06-06 09:38:45 -0500

    fixed weight issue.

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -6,7 +6,7 @@ function randn_bm() {
     var u = 0, v = 0;
     while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
     while(v === 0) v = Math.random();
-    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )-0.5;
 }
 
 
