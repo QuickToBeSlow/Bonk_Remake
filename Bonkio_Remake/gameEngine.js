79441e04113c75a57ca09817941c702036110917
Blake <35315543+QuickToBeSlow@users.noreply.github.com>
2020-06-27 20:47:13 -0500

    weights now can increase..

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -172,7 +172,7 @@
 			  for (let j = 0; j < values.length; j++) {
 				if (Math.random() < rate) {
 				  let w = values[j];
-				  values[j] = (Math.abs(w) > 0.01) ? w + randn_bm()*w : randn_bm;
+				  values[j] = (Math.abs(w) > 0.01) ? w + randn_bm()*w*2 : randn_bm;
 				}
 			  }
 			  let newTensor = tf.tensor(values, shape);
