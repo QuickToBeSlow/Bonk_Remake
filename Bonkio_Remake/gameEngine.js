a717114f297ba2cf117b4d20bdd148908c00a414
Blake <35315543+QuickToBeSlow@users.noreply.github.com>
2020-06-27 20:16:57 -0500

    now preventing negative weights.

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -6,7 +6,7 @@
 		var u = 0, v = 0;
 		while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
 		while(v === 0) v = Math.random();
-		return (Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v ))-0.5;
+		return (Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v ));
 	}
 	
 	function sigmoid(value) {
