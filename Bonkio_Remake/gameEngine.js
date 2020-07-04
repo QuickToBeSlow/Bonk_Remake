4fb18f1cfb5fa0f4d4b0f8065bd9256b61ab6a73
Blake <35315543+QuickToBeSlow@users.noreply.github.com>
2020-07-04 08:09:50 -0500

    fixed random gaussian issue.

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -6,7 +6,10 @@
 		var u = 0, v = 0;
 		while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
 		while(v === 0) v = Math.random();
-		return (Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v ))-0.5;
+		let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+		num = num / 10.0 + 0.5; // Translate to 0 -> 1
+		if (num > 1 || num < 0) return randn_bm(); // resample between 0 and 1
+		return num;
 	}
 	
 	function sigmoid(value) {
