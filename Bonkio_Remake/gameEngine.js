8b556f0de49e9a5f6bf29acd8349fb2789c1d45c
willblakemeyer <willblakemeyer@gmail.com>
2021-09-02 08:08:54 -0500

    Changed structure of NNs again

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -115,7 +115,7 @@
 		  if (brain) {
 			this.brain = brain.copy();
 		  } else {
-			this.brain = new NeuralNetwork(12+(window.eyes*2), [7, 5], 3);
+			this.brain = new NeuralNetwork(12+(window.eyes*2), [9, 7, 5], 3);
 		  }
 		}
 	  
@@ -124,7 +124,7 @@
 		}
 	
 		mutate() {
-		  this.brain.mutate(0.01);
+		  this.brain.mutate(0.0035);
 		}
 	  
 		think(i) {
