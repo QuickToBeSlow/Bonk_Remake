ce6647fee7f2fdb3433713db03d2ec28c184b44c
willblakemeyer <willblakemeyer@gmail.com>
2021-09-10 22:48:02 -0500

    Fixed sigmoid function again...

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -47,14 +47,14 @@
 		while(v === 0) v = Math.random();
 		let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
 		if (restrict == true) {
-			num = num / 10.0 + 0.5; // Translate to 0 -> 1
+			num = num / 10.0 /*+ 0.5*/; // Translate to 0 -> 1
 			if (num > 1 || num < 0) return randn_bm(true); // resample between 0 and 1
 		}
 		return num;
 	}
 	
 	function sigmoid(value) {
-		return (Math.pow(Math.E, value)/(1+Math.pow(Math.E, value)))-0.5;
+		return (Math.pow(Math.E, value)/(1+Math.pow(Math.E, value)));
 	}
 	//converts inputs to sigmoid values (this is used for the inputs!)
 
@@ -336,10 +336,7 @@
 				if (Math.random() < rate) {
 				  let w = values[j];
 				//   values[j] = (Math.abs(w) > 0) ? (w + randn_bm()*w) : w + randn_bm()+0.5;
-				  values[j] = w + (randn_bm(false)-0.5);
-				  if (values[j]<0.0001) {
-					values[j] = 0.0001;
-				  }
+				  values[j] = w + (randn_bm(false));
 				}
 			  }
 			  let newTensor = tf.tensor(values, shape);
