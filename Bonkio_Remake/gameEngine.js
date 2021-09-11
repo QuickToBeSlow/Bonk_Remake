01d2037464ca7f6af6c22cea71e734a8175c9e84
willblakemeyer <willblakemeyer@gmail.com>
2021-09-10 21:39:11 -0500

    Prevented weights from continuously growing! AGH!

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -336,7 +336,7 @@
 				if (Math.random() < rate) {
 				  let w = values[j];
 				//   values[j] = (Math.abs(w) > 0) ? (w + randn_bm()*w) : w + randn_bm()+0.5;
-				  values[j] = w + randn_bm(false);
+				  values[j] = w + (randn_bm(false)-0.5);
 				}
 			  }
 			  let newTensor = tf.tensor(values, shape);
