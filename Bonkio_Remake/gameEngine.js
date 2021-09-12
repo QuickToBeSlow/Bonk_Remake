e43a7f7113839e03541e7a0939ae07428580486f
willblakemeyer <willblakemeyer@gmail.com>
2021-09-11 20:56:48 -0500

    lowered mutation rate, changed randn stdev

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -105,7 +105,7 @@
 		while(v === 0) v = Math.random();
 		let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
 		if (restrict == true) {
-			num = num/10; // Translate to 0 -> 1
+			num = num / 2; // Translate to 0 -> 1
 			if (num > 1 || num < 0) return randn_bm(true); // resample between 0 and 1
 		}
 		return num;
@@ -188,7 +188,7 @@
 		}
 	
 		mutate() {
-		  this.brain.mutate(0.01);
+		  this.brain.mutate(0.0025);
 		}
 	  
 		think(i) {
