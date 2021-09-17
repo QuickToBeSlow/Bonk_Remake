7334e7ea6d8972d2b9d9577355df547901f4c064
willblakemeyer <willblakemeyer@gmail.com>
2021-09-17 17:06:00 -0500

    Changed mutation settings

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -105,8 +105,8 @@
 		while(v === 0) v = Math.random();
 		let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
 		if (restrict == true) {
-			num = num / 10; // Translate to 0 -> 1
-			if (num > 1 || num < 0) return randn_bm(true); // resample between 0 and 1
+			num = num / 2; // Translate to 0 -> 1
+			// if (num > 1 || num < 0) return randn_bm(true); // resample between 0 and 1
 		}
 		return num;
 	}
@@ -197,7 +197,7 @@
 		}
 	
 		mutate() {
-		  this.brain.mutate(0.005);
+		  this.brain.mutate(0.01);
 		}
 	  
 		think(i) {
