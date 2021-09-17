1e563e32674612d14f548f622439a1af4d0721c2
willblakemeyer <willblakemeyer@gmail.com>
2021-09-17 13:11:43 -0500

    changed some settings for mutations

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -105,7 +105,7 @@
 		while(v === 0) v = Math.random();
 		let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
 		if (restrict == true) {
-			num = num / 2; // Translate to 0 -> 1
+			num = num / 10; // Translate to 0 -> 1
 			if (num > 1 || num < 0) return randn_bm(true); // resample between 0 and 1
 		}
 		return num;
@@ -197,7 +197,7 @@
 		}
 	
 		mutate() {
-		  this.brain.mutate(0.0025);
+		  this.brain.mutate(0.005);
 		}
 	  
 		think(i) {
@@ -405,7 +405,7 @@
 				if (Math.random() < rate) {
 				  let w = values[j];
 				//   values[j] = (Math.abs(w) > 0) ? (w + randn_bm()*w) : w + randn_bm()+0.5;
-				  values[j] = w + (randn_bm(true));
+				  values[j] = w + (randn_bm(false));
 				}
 			  }
 			  let newTensor = tf.tensor(values, shape);
