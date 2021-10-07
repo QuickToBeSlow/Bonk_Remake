9a02927df01a82095f11f8d8d3e46a1f9beeaf33
willblakemeyer <willblakemeyer@gmail.com>
2021-10-07 17:07:03 -0500

    Changed starting eyeRotation

--- Bonkio_Remake/gameEngine.js
+++ Bonkio_Remake/gameEngine.js
@@ -196,7 +196,7 @@
 		}
 	  
 		crossover(network) {
-			this.brain.crossover(network.brain.model, 0.05);
+			this.brain.crossover(network.brain.model, 0.15);
 		}
 
 		think(i) {
@@ -219,7 +219,7 @@
 				// let change = 360/(window.eyes)/180*Math.PI;
 				for (let m=0; m<window.eyes; m++) {
 					// eyeRotation[0][m] = (this.lastOutputs[m]*2-1.5+1*m%2)*Math.PI;
-					eyeRotation[0][m] = (this.lastOutputs[m]*2-1)*Math.PI;
+					eyeRotation[0][m] = (this.lastOutputs[m]*2-1)*Math.PI-Math.PI/2;
 					let cast = raycast(window.FloorFixture, new b2Vec2(PPosX, PPosY), new b2Vec2(PPosX+(Math.cos((eyeRotation[0][m]))*eyeRange), PPosY-(Math.sin((eyeRotation[0][m]))*eyeRange)));
 					inputs[8+this.lastOutputs.length+m] = (cast.distance || -eyeRange)/eyeRange;
 					inputs[8+this.lastOutputs.length+m+window.eyes] = cast.angle || 0;
@@ -293,7 +293,7 @@
 				// let change = 360/(window.eyes)/180*Math.PI;
 				for (let m=0; m<window.eyes; m++) {
 					// eyeRotation[0][m] = (this.lastOutputs[m]*2-1.5+1*m%2)*Math.PI;
-					eyeRotation[1][m] = (this.lastOutputs[m]*2-1)*Math.PI;
+					eyeRotation[1][m] = (this.lastOutputs[m]*2-1)*Math.PI-Math.PI/2;
 					let cast = raycast(window.FloorFixture, new b2Vec2(PPosX, PPosY), new b2Vec2(PPosX+(Math.cos((eyeRotation[1][m]))*eyeRange), PPosY-(Math.sin((eyeRotation[1][m]))*eyeRange)));
 					inputs[8+this.lastOutputs.length+m] = (cast.distance || -eyeRange)/eyeRange;
 					inputs[8+this.lastOutputs.length+m+window.eyes] = cast.angle || 0;
@@ -1091,7 +1091,7 @@
 						// let change = 360/(window.eyes)/180*Math.PI;
 						for (let m=0; m<window.eyes; m++) {
 							// eyeRotation[0][m] = (lastOutputs[m]*2-1.5+1*m%2)*Math.PI;
-							eyeRotation[0][m] = (lastOutputs[m]*2-1)*Math.PI;
+							eyeRotation[0][m] = (lastOutputs[m]*2-1)*Math.PI-Math.PI/2;
 							let cast = raycast(window.FloorFixture, new b2Vec2(PPosX, PPosY), new b2Vec2(PPosX+(Math.cos((eyeRotation[0][m]))*eyeRange), PPosY-(Math.sin((eyeRotation[0][m]))*eyeRange)));
 							inputs[8+lastOutputs.length+m] = (cast.distance || -eyeRange)/eyeRange;
 							inputs[8+lastOutputs.length+m+window.eyes] = cast.angle || 0;
@@ -1164,7 +1164,7 @@
 						// let change = 360/(window.eyes)/180*Math.PI;
 						for (let m=0; m<window.eyes; m++) {
 							// eyeRotation[0][m] = (lastOutputs[m]*2-1.5+1*m%2)*Math.PI;
-							eyeRotation[1][m] = (lastOutputs[m]*2-1)*Math.PI;
+							eyeRotation[1][m] = (lastOutputs[m]*2-1)*Math.PI-Math.PI/2;
 							let cast = raycast(window.FloorFixture, new b2Vec2(PPosX, PPosY), new b2Vec2(PPosX+(Math.cos((eyeRotation[1][m]))*eyeRange), PPosY-(Math.sin((eyeRotation[1][m]))*eyeRange)));
 							inputs[8+lastOutputs.length+m] = (cast.distance || -eyeRange)/eyeRange;
 							inputs[8+lastOutputs.length+m+window.eyes] = cast.angle || 0;
