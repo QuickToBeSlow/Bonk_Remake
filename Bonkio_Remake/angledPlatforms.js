b9ddb27740d979096ee8db5ae12c8d83ef9124d2
willblakemeyer <willblakemeyer@gmail.com>
2021-10-12 09:34:17 -0500

    Added maps, changed pause/resume, testing new map

---
+++ Bonkio_Remake/angledPlatforms.js
@@ -0,0 +1,113 @@
+(function(){
+
+var ctx = document.getElementById("canvas");
+
+var Test = function() {
+	gameEngine.__constructor.apply(this, arguments);
+};
+extend(Test.prototype, gameEngine.prototype)
+
+Test.prototype.createWorld = function() {
+	var world = gameEngine.prototype.createWorld.apply(this, arguments);
+	var m_physScale = 10;
+	window.shapes = [];
+	var fd = new b2FixtureDef();
+	// var fd2 = new b2FixtureDef();
+	var sd = new b2PolygonShape();
+	var sd2 = new b2PolygonShape();
+	var bd = new b2BodyDef();
+	bd.type = b2Body.b2_staticBody;
+	bd.fixedRotation = true;
+	//bd.isBullet = true;
+	var b;
+	fd.density = 1.0;
+	fd.friction = 0.0;
+	fd.restitution = 0.8;
+	fd.shape = sd;	
+
+	let platWidths = 20;
+	let platHeights = 5;
+	let distanceX = 500;
+	let distanceX = 300;
+	let platNumX = 7;
+	let platNumY = 3;
+	let xChange = 10;
+	let yChange = 10;
+	let angleChange = Math.PI/8;
+	for (let j=platWidths; j<distanceX; j+=distance/platNumX) {
+		for (let i=0; i<distanceY; i+=distanceY/platNumY) {
+			sd.SetAsBox((platWidths) / m_physScale, (platHeights) / m_physScale);
+			fd.shape = sd;
+			fd.density = 0;
+			bd.type = b2Body.b2_staticBody;
+			bd.userData = "Floor";
+			b.SetAngle(Math.random()*angleChange-angleChange/2);
+			bd.position.Set((j+Math.random()*xChange-xChange/2) / m_physScale, (i+Math.random()*yChange-yChange/2) / m_physScale);
+			b = world.CreateBody(bd);
+			window.FloorFixture = b.CreateFixture(fd);
+			window.shapes.push(b.CreateFixture(fd));
+		}
+	}
+	//Random Pos
+	let randPos = (Math.round(Math.random())-0.5)*2;
+	
+	// Create ball
+	var P1 = new b2CircleShape();
+	P1.m_radius = 15/m_physScale;
+	fd.density = 1;
+	fd.restitution = 0.8;
+	fd.friction = 0.0;
+	fd.shape = P1;
+	bd.type = b2Body.b2_dynamicBody;
+	bd.userData = "Player1";
+	bd.isBullet = true;
+// 	if (randPos == 0) {
+// 		bd.position.Set(225/m_physScale, 120/m_physScale);
+// 	} else {
+// 		bd.position.Set(375/m_physScale, 120/m_physScale);
+// 	}
+	bd.position.Set((300+randPos*100)/m_physScale, (350+Math.round(Math.random()*25))/m_physScale);
+	// bd.position.Set(275/m_physScale, 120/m_physScale);
+	window.Player1 = world.CreateBody(bd);
+	window.PFixture1 = window.Player1.CreateFixture(fd);
+	sd2.SetAsBox(5,5, new b2Vec2(0, 0), 0);
+	sd2.m_vertices = [new b2Vec2(-0.2, 0), new b2Vec2(0.2, 0), new b2Vec2(0.2, -1.55), new b2Vec2(-0.2, -1.55)];
+	fd.density = 0;
+	fd.isSensor = true;
+	fd.shape = sd2;
+	window.P1Foot = window.Player1.CreateFixture(fd);
+	window.P1Foot.SetUserData("Foot1");
+
+	var P2 = new b2CircleShape();
+	P2.m_radius = 15/m_physScale;
+// 	if (randPos == 1) {
+// 		bd.position.Set(225/m_physScale, 120/m_physScale);
+// 	} else {
+// 		bd.position.Set(375/m_physScale, 120/m_physScale);
+// 	}	fd.density = 1;
+	fd.restitution = 0.8;
+	fd.friction = 0.0;
+	fd.isSensor = false;
+	fd.shape = P2;
+	bd.position.Set((300-randPos*100)/m_physScale, (350+Math.round(Math.random()*25))/m_physScale);
+	// bd.position.Set(325/m_physScale, 120/m_physScale);
+	bd.userData = "Player2";
+	window.Player2 = world.CreateBody(bd);
+	window.PFixture2 = window.Player2.CreateFixture(fd);
+	// sd2.SetAsBox(5,5, new b2Vec2(0, 0), 0);
+	// sd2.m_vertices = [new b2Vec2(-0.2, 0), new b2Vec2(0.2, 0), new b2Vec2(0.2, -1.75), new b2Vec2(-0.2, -1.75)];
+	fd.density = 0;
+	fd.isSensor = true;
+	fd.shape = sd2;
+	window.P2Foot = window.Player2.CreateFixture(fd);
+	window.P2Foot.SetUserData("Foot2");
+	// console.log(Player2);
+
+
+
+	return world;
+};
+
+window.angledPlatforms = Test;
+	
+})();
